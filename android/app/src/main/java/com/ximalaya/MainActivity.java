2f12a8680f3d6aa9b58d24d74f63f07581c63512
renjie_cai <renjie.cai@ihoment.com>
2022-03-22 18:28:40 +0800

    change ts project

--- android/app/src/main/java/com/ximalaya/MainActivity.java
+++ android/app/src/main/java/com/ximalaya/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rndemo;
+package com.ximalaya;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "rnDemo";
+    return "ximalaya";
   }
 }
